d5c278c6fed60b218c7fc71645527820995286ca
ha-manel <manelhammouche3@gmail.com>
2022-09-20 14:01:00 +0100

    Create routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Greeting from './components/Greeting';
 
 function App() {
   return (
-    <div>
-    </div>
+    <Routes>
+      <Route path="/" element={<Greeting />} />
+    </Routes>
   );
 }
 
